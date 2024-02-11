8ca9d756d9118b08ab28ccd97211f0bf7bd6e1a5
Sammer55 <sammerduarte@hotmail.com>
2024-02-11 00:04:31 -0300

    🎉: start

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,18 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      "transform-inline-environment-variables",
+      [
+        "@tamagui/babel-plugin",
+        {
+          components: ["tamagui"],
+          config: "./tamagui.config.ts",
+          logTimings: true,
+        },
+      ],
+      "react-native-reanimated/plugin",
+    ],
   };
 };
